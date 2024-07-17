211e7a0bca555c1af684b43038832410f0f24618
mostafaosama26 <drsh26.works@gmail.com>
2024-07-17 09:18:41 +0300

    Initial commit.

---
+++ lib/main.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const Islami());
+}
+
+class Islami extends StatelessWidget {
+  const Islami({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+
+    );
+  }
+}
